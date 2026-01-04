be910873ec4a0b0c80d8106a4c9df62b196c6ef1
axwinachu <ceaswin146@gmail.com>
2026-01-04 21:25:58 +0530

    celling of the number using binary search approach

---
+++ CellingOfTheNumber.java
@@ -0,0 +1,19 @@
+public class CellingOfTheNumber {
+    public static void main(String[] args) {
+        int[] arr={1,3,4,5,67,78,90,100};
+        int target=15;
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                System.out.println(arr[mid]);
+            }else if(arr[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        System.out.println(arr[start]);
+    }
+}
